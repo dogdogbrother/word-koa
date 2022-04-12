7e973fcfcc97ea15e51657c2ff1cd74978931f46
森林 <gougewh@sina.com>
2022-04-12 10:33:49 +0800

    增加数据库同步文件

---
+++ src/db/force-sync.js
@@ -0,0 +1,17 @@
+const seq = require('./seq')
+require('../models/index')
+// 测试连接
+seq.authenticate().then(() => {
+  console.log('auth ok')
+}).catch(() => {
+  console.log('auth err')
+})
+
+/**
+ * @description 执行同步
+ * @param Object alter 会尽量的去修正表的内容,force 会强制重置表内容
+ */
+seq.sync({ alter: true }).then(() => {
+  console.log('sync ok')
+  process.exit()
+})
